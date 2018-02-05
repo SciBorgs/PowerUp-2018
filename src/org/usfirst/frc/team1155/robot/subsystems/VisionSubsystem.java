f117242092b84d9651a9a37518c81bcb0da0b874
Alexander Warren <arocks124@gmail.com>
2018-02-05 15:16:27 -0500

    Added Vision Subsystem

---
+++ src/org/usfirst/frc/team1155/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team1155.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ *
+ */
+public class VisionSubsystem extends Subsystem {
+    
+    public boolean canSeeCube(){
+    	return SmartDashboard.getBoolean("canSeeCube", false);
+    }
+    
+    public double getAngleToCube(){
+    	return SmartDashboard.getNumber("angleToCube", 0);
+    }
+    
+    public double getDistanceToCube(){
+    	return SmartDashboard.getNumber("distanceToCube", 0);
+    }
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+		
+	}
+    
+}
+
