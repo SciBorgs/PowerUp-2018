9ba7af29753b083ba793a3ede9ccbacaa9092c7f
Alexander Warren <arocks124@gmail.com>
2018-02-08 15:59:48 -0500

    Added loading from file

--- src/org/usfirst/frc/team1155/robot/subsystems/AutonomousSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/AutonomousSubsystem.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1155.robot.subsystems;
 
+import java.io.File;
+import java.io.IOException;
+
 import api.AutonomousRoutine;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -12,47 +15,14 @@ public class AutonomousSubsystem extends Subsystem {
 	
 	
 	public AutonomousRoutine configurePath(String gameInfo, int pos){
-		switch(pos){
-		case 1:
-			switch(gameInfo){
-			case "LLL":
-				break;
-			case "RRR":
-				break;
-			case "LRL":
-				break;
-			case "RLR":
-				break;
-			}
-			break;
-		case 2:
-			switch(gameInfo){
-			case "LLL":
-				break;
-			case "RRR":
-				break;
-			case "LRL":
-				break;
-			case "RLR":
-				break;
-			}
-			break;
-		case 3:
-			switch(gameInfo){
-			case "LLL":
-				break;
-			case "RRR":
-				break;
-			case "LRL":
-				break;
-			case "RLR":
-				break;
-			}
-			break;
-		default:
-			break;
+		
+		try {
+			return new AutonomousRoutine(new File("/home/lvuser/AutoRoutines/" + pos + "/" + gameInfo));
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
 		}
-		return new AutonomousRoutine();
+		
 	}
 	
     public void initDefaultCommand() {
