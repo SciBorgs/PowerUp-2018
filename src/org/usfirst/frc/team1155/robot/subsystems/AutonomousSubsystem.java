2f4fbd76e7c0fadbee6e920ebcf58a486684d196
Alexander Warren <arocks124@gmail.com>
2018-03-29 16:59:17 -0400

    Added auto paths

--- src/org/usfirst/frc/team1155/robot/subsystems/AutonomousSubsystem.java
+++ src/org/usfirst/frc/team1155/robot/subsystems/AutonomousSubsystem.java
@@ -20,32 +20,156 @@ public class AutonomousSubsystem extends Subsystem {
 		gameInfo = gameInfo.substring(0, 2);
 		
 		//0 = left, 1 = middle, 2 = right
-		String name = "";
+		String info = "";
 		switch(pos){
 		case 0:
-			name += "Left-";
+			info += "Left-";
 			break;
 		case 1:
-			name += "Middle-";
+			info += "Middle-";
 			break;
 		case 2:
-			name += "Right-";
+			info += "Right-";
 			break;
 		}
 		
-		name += gameInfo + "-";
-		name += priority;
-		AutonomousRoutine path = getPathWithName(name);
+		info += gameInfo + "-";
+		info += priority;
+		AutonomousRoutine path = getPathWithInfo(info);
 		return path;
 	}
-	
-	public AutonomousRoutine getPathWithName(String name){
-		AutonomousRoutine path = new AutonomousRoutine();
 
-		switch(name){
+	public AutonomousRoutine getPathWithInfo(String info){
+
+		switch(info){
 		case "Left-LL-Switch":
-			
+		case "Left-LR-Switch":
+		case "Left-LL-Either":
+		case "Left-LR-Either":
+			return getPathFromName("LeftPos-LeftSwitch");
+		case "Left-LL-Scale":
+		case "Left-RL-Scale":
+		case "Left-RL-Either":
+			return getPathFromName("LeftPos-LeftScale");
+		case "Left-RL-Switch":
+		case "Left-RR-Switch":
+		case "Left-RR-Either":
+			return getPathFromName("LeftPos-RightSwitch");
+		case "Left-LR-Scale":
+		case "Left-RR-Scale":
+			return getPathFromName("LeftPos-RightScale");
+		case "Right-RR-Switch":
+		case "Right-RL-Switch":
+		case "Right-RR-Either":
+		case "Right-RL-Either":
+			return getPathFromName("RightPos-RightSwitch");
+		case "Right-RR-Scale":
+		case "Right-LR-Scale":
+		case "Right-LR-Either":
+			return getPathFromName("RightPos-RightScale");
+		case "Right-LR-Switch":
+		case "Right-LL-Switch":
+		case "Right-LL-Either":
+			return getPathFromName("RightPos-LeftSwitch");
+		case "Right-RL-Scale":
+		case "Right-LL-Scale":
+			return getPathFromName("RightPos-LeftScale");
+		case "Middle-LL-Switch":
+		case "Middle-LR-Switch":
+		case "Middle-LL-Either":
+		case "Middle-LR-Either":
+			return getPathFromName("MiddlePos-LeftSwitch");
+		case "Middle-RL-Switch":
+		case "Middle-RR-Switch":
+		case "Middle-RL-Either":
+		case "Middle-RR-Either":
+			return getPathFromName("MiddlePos-RightSwitch");
+		case "Middle-LL-Scale":
+		case "Middle-RL-Scale":
+			return getPathFromName("MiddlePos-LeftScale");
+		case "Middle-LR-Scale":
+		case "Middle-RR-Scale":
+			return getPathFromName("MiddlePos-RightScale");
+		default:
+			return getPathFromName("Baseline");
 		}
+	}
+	
+	public AutonomousRoutine getPathFromName(String name){
+		AutonomousRoutine path = new AutonomousRoutine();
+		switch(name){
+		case "LeftPos-LeftSwitch":
+			path.add(1.2083333333333333, 6.208333333333333, null);
+			path.add(13.791666666666666, 6.166666666666667, null);
+			path.add(13.791666666666666, 7.458333333333333, null);
+			path.add(13.791666666666666, 8.333333333333334, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+		case "LeftPos-LeftScale":
+			path.add(1.0416666666666667, 6.291666666666667, null);
+			path.add(26.875, 6.208333333333333, null);
+			path.add(26.875, 7.125, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		case "LeftPos-RightSwitch":
+			path.add(1.0416666666666667, 6.291666666666667, null);
+			path.add(19.291666666666668, 6.25, null);
+			path.add(19.416666666666668, 20.083333333333332, null);
+			path.add(17.791666666666668, 20.083333333333332, null);
+			path.add(16.25, 20.083333333333332, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+		case "LeftPos-RightScale":
+			path.add(1.0416666666666667, 6.291666666666667, null);
+			path.add(19.333333333333332, 6.291666666666667, null);
+			path.add(19.5, 24.0, null);
+			path.add(26.875, 23.916666666666668, null);
+			path.add(26.875, 23.208333333333332, null);
+			path.add(26.875, 21.875, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		case "RightPos-RightSwitch":
+			path.add(1.75, 23.5, null);
+			path.add(13.958333333333334, 23.458333333333332, null);
+			path.add(13.958333333333334, 22.25, null);
+			path.add(13.958333333333334, 20.916666666666668, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+		case "RightPos-RightScale":
+			path.add(1.7083333333333333, 23.541666666666668, null);
+			path.add(27.041666666666668, 23.5, null);
+			path.add(27.083333333333332, 21.875, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		case "RightPos-LeftSwitch":
+			path.add(1.7083333333333333, 23.5, null);
+			path.add(19.791666666666668, 23.5, null);
+			path.add(19.833333333333332, 9.958333333333334, null);
+			path.add(17.916666666666668, 9.958333333333334, null);
+			path.add(15.791666666666666, 9.958333333333334, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+		case "RightPos-LeftScale":
+			path.add(1.75, 23.541666666666668, null);
+			path.add(19.75, 23.541666666666668, null);
+			path.add(19.875, 5.166666666666667, null);
+			path.add(26.916666666666668, 5.25, null);
+			path.add(27.0, 7.958333333333333, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		case "MiddlePos-LeftSwitch":
+			path.add(1.2916666666666667, 14.916666666666666, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+			path.add(5.916666666666667, 14.916666666666666, null);
+			path.add(5.958333333333333, 10.083333333333334, null);
+			path.add(10.083333333333334, 10.041666666666666, null);
+			path.add(12.208333333333334, 10.0, null);
+		case "MiddlePos-RightSwitch":
+			path.add(1.2916666666666667, 14.958333333333334, null);
+			path.add(5.875, 14.916666666666666, null);
+			path.add(5.916666666666667, 19.5, null);
+			path.add(10.166666666666666, 19.541666666666668, null);
+			path.add(12.708333333333334, 19.458333333333332, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SWITCH);
+		case "MiddlePos-LeftScale":
+			path.add(1.4166666666666667, 14.791666666666666, null);
+			path.add(5.541666666666667, 14.791666666666666, null);
+			path.add(5.583333333333333, 5.208333333333333, null);
+			path.add(26.875, 5.708333333333333, null);
+			path.add(26.875, 8.375, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		case "MiddlePos-RightScale":
+			path.add(1.375, 14.791666666666666, null);
+			path.add(5.541666666666667, 14.791666666666666, null);
+			path.add(5.75, 23.708333333333332, null);
+			path.add(26.958333333333332, 23.75, null);
+			path.add(26.791666666666668, 21.208333333333332, AutonomousRoutine.AutonomousAction.PLACE_CUBE_ON_SCALE);
+		default:
+			path.add(1.4166666666666667, 14.833333333333334, null);
+			path.add(10.083333333333334, 14.791666666666666, null);
+		}
+		
 		return path;
 	}
 	
